763b71190c336eb5bf4697e3b10728eaf77e47f2
Woorim Kim <wrkimlimi@gmail.com>
2024-05-23 22:43:18 +0900

    code for react session

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import WritePost from "./components/WritePost/WritePost";
+import Home from "./components/Home/Home";
+import PostDetail from "./components/PostDetail/PostDetail";
+import EditPost from "./components/EditPost/EditPost";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/write" element={<WritePost />} />
+          <Route path="/post/:id" element={<PostDetail />} />
+          <Route path="/edit/:id" element={<EditPost />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
